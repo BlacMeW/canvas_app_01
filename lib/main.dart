4b0fafa493bc5023335051d9fa8087fdd6080bd4
BlacMeW <mybooksdata2024@gmail.com>
2025-07-01 19:48:38 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Solar System with Jean Meeus',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const SolarSystemPage(),
     );
